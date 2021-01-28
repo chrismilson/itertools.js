af85accc1fc8854d2f9215d3f6acaeec13c62320
Chris Milson <chris.w.milson@gmail.com>
2021-01-28 09:23:19 +0900

    Remove spreadmap
    The spreadmap method can easily be implemented by the end user with the
    `map` method instead.

--- src/__tests__/map.test.ts
+++ src/__tests__/map.test.ts
@@ -1,4 +1,4 @@
-import { map, spreadMap } from '..'
+import { map } from '..'
 
 describe('map', () => {
   it('Should apply the map to each value in the iterator', () => {
@@ -27,50 +27,5 @@ describe('map', () => {
     const mapper = Math.pow
 
     expect([...map(mapper, a, b)]).toMatchObject([1, 4, 9, 16])
-  })
-})
-
-describe('spreadMap', () => {
-  it('Should apply the map to each value in the iterator', () => {
-    const values = [1, 2, 3, 4, 5]
-    const mapper = (v: number): number => v * 2
-
-    expect([
-      ...spreadMap<number[], number>(
-        mapper,
-        values.map((v) => [v])
-      ),
-    ]).toMatchObject(values.map(mapper))
-  })
-
-  it('Should apply the map to each value only once', () => {
-    let count = 0
-    const values = [1, 2, 3, 4, 5]
-    const mapper = (v: number): number => v * 2
-    const counter = (v: number): number => {
-      count += 1
-      return mapper(v)
-    }
-
-    expect([
-      ...spreadMap<number[], number>(
-        counter,
-        values.map((v) => [v])
-      ),
-    ]).toMatchObject(values.map(mapper))
-    expect(count).toBe(values.length)
-  })
-
-  it('Should apply the map to multiple arguments', () => {
-    const a = [1, 2, 3, 4]
-    const b = [2, 2, 2, 2]
-    const mapper = Math.pow
-
-    expect([
-      ...spreadMap<number[], number>(
-        mapper,
-        a.map((v, i) => [v, b[i]])
-      ),
-    ]).toMatchObject([1, 4, 9, 16])
   })
 })
