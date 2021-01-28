556ad94123e4f9b7e754f8b8701f9d64a70413b0
Chris Milson <chris.w.milson@gmail.com>
2021-01-28 09:22:28 +0900

    Update package declaration

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,18 @@
 /* global require */
 import typescript from 'rollup-plugin-typescript2'
+import pkg from './package.json'
 
 export default {
-  input: "src/index.ts",
-  output: {
-      dir: "./dist"
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.module,
+      format: 'es'
     }
-  ,
-  plugins: [typescript({ typescript: require("typescript") })]
+  ],
+  plugins: [typescript({ typescript: require('typescript') })]
 }
